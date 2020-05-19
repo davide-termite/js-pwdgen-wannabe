2be38df6fa5a9da26efa1d4c745c215beac8eb82
Davide Termite <davide.termite@gmail.com>
2020-05-19 17:17:42 +0200

    Script Completed

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// Richiesta Nome
+var name = prompt("Qual è il tuo nome?")
+
+// Richiesta Cognome
+var secondName = prompt("Qual è il tuo cognome?")
+
+// Richiesta Colore
+var favColor = prompt("Qual è il tuo colore preferito?")
+
+// Password Generator
+var password = name + secondName + favColor + "19"
+
+var result = "Ciao " + name + "! La tua password è " + password
